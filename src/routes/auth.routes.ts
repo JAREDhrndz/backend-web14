0ab7764004a5e951962909112955c67b9cc1ce8b
JAREDhrndz <jaredhdz5834@gmail.com>
2025-07-20 22:08:06 -0600

    Backend commit

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,16 +1,25 @@
 // src/routes/auth.routes.ts
-import {Router} from 'express'
-import {getAllUsers, getTimeToken, getUserByUsername, login, saveUser, updateToken, updateUser, deleteUser} from "../controllers/auth.controller"
+import { Router, Request, Response } from 'express';
+import {
+  getAllUsers,
+  getTimeToken,
+  getUserByUsername,
+  login,
+  saveUser,
+  updateToken,
+  updateUser,
+  deleteUser
+} from "../controllers/auth.controller";
 
 const router = Router();
 
-router.post('/login-user', login);
-router.get('/getTime/:userId', getTimeToken);
-router.patch('/update/:userId', updateToken);
-router.get('/users', getAllUsers);
-router.post('/users', saveUser);
-router.get('/users/name/:userName', getUserByUsername);
-router.patch('/users/:userId', updateUser);
-router.delete('/users/:userId', deleteUser);
+router.post('/login-user', (req: Request, res: Response) => login(req, res));
+router.get('/getTime/:userId', (req: Request, res: Response) => getTimeToken(req, res));
+router.patch('/update/:userId', (req: Request, res: Response) => updateToken(req, res));
+router.get('/users', (req: Request, res: Response) => getAllUsers(req, res));
+router.post('/users', (req: Request, res: Response) => saveUser(req, res));
+router.get('/users/name/:userName', (req: Request, res: Response) => getUserByUsername(req, res));
+router.patch('/users/:userId', (req: Request, res: Response) => updateUser(req, res));
+router.delete('/users/:userId', (req: Request, res: Response) => deleteUser(req, res));
 
 export default router;
